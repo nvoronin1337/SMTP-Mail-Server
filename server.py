ccf24150099a3a6dbe37eb80dfcd788885b34a1b
Nikita Voronin <voro0612@gmail.com>
2019-11-11 15:04:06 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from aiosmtpd.controller import Controller
 import asyncio
 from aiosmtpd.smtp import SMTP as Server, syntax
+import ssl
 
 
 class SMTPServer(Server):
@@ -8,35 +9,47 @@ class SMTPServer(Server):
     async def smtp_PING(self, arg):
         await self.push('259 Pong')
 
-    @syntax("AUTH USERNAME PASSWORD")
+    @syntax("AUTH USERNAME PASSWORD [ignored]")
     async def smtp_AUTH(self, arg):
         if arg is None:
-            await self.push('501 Syntax: AUTH USERNAME')
+            await self.push('501 Syntax: AUTH USERNAME PASSWORD [ignored]')
             return
         else:
             credentials = arg.split(' ')
             if len(credentials) is not 2:
-                await self.push('501 Syntax: AUTH USERNAME')
+                await self.push('501 Syntax: AUTH USERNAME PASSWORD [ignored]')
                 return
             else:
-                #check if data is in db
                 if credentials[0] == 'username' and credentials[1] == 'password':
                     self.authenticated = True
                     await self.push('253 Authentication successful')
                 else:
                     await self.push('535 Invalid credentials')
 
-    @syntax('MAIL FROM: <address@example.com>')
+    @syntax('MAIL FROM: <address@project.com>')
     async def smtp_MAIL(self, arg):
         if(arg is None):
             await self.push('501 Syntax: MAIL FROM: <address@example.com>')
         else:
             if(arg[7:len(arg)-1].endswith('@project.com')):
-                await self.push('250 OK')
+                self.envelope.mail_from = arg[7:len(arg)-1]
+                message = '250 OK MAIL FROM: ' + arg[7:len(arg)-1]
+                await self.push(message)
             else:
                 await self.push('551 not relaying to that domain')
+                
+    @syntax('RCPT TO: <address@project.com')
+    async def smtp_RCPT(self, arg):
+        if arg is None:
+            await self.push('501 Syntax: RCPT TO: <address@example.com>')
+        else:
+            self.envelope.rcpt_tos = arg[5:len(arg)-1]
+            message = '250 OK RCPT TO: ' + arg[5:len(arg)-1]
+            await self.push(message)
 
     
 class MyController(Controller):
     def factory(self):
-        return SMTPServer(self.handler)
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain('cert.cert', 'key.key')
+        return SMTPServer(self.handler, tls_context=context, require_starttls=True)
--- tests.py
+++ tests.py
@@ -7,39 +7,52 @@ def test_HELO(client):
     code, message = client.helo('host1')
     assert(str(code) == '250')
 
-
 def test_PING(client):
     code, message = client.docmd('PING')
     assert(str(code) == '259')
-
 
 def test_AUTH(client):
     code, message = client.docmd('AUTH username password')
     assert(str(code) == '253')
 
-
 def test_MAIL(client):
-    code, message = client.docmd('MAIL FROM: <asdaskek@project.com>')
+    code, message = client.docmd('MAIL FROM: <example@project.com>')
     assert(str(code) == '250')
-    code, message = client.docmd('MAIL FROM: <asdaskek@wrong.com>')
-    assert(str(code) == '551')
-
 
 def test_RCPT(client):
-    code, message = client.docmd('RCPT TO: kek@project.com')
+    code, message = client.docmd('RCPT TO: <example@project.com>')
     assert(str(code) == '250')
 
+def test_DATA(client):
+    pass
+
+def test_sendmail(client):
+    client.sendmail('example@project.com', 'example2@project.com','hello there')
+    print('---------------------------------------------')
 
 def run_tests():
     controller = MyController(Sink())
     controller.start()
+
     client = Client(controller.hostname, controller.port)
+    client.starttls()
+    client.set_debuglevel(True)
 
     test_HELO(client)
     test_PING(client)
     test_AUTH(client)
-    test_MAIL(client)
-    #test_RCPT(client)
+    test_sendmail(client)
+    print('client 1 end')
+
+    client2 = Client(controller.hostname, controller.port)
+    client2.starttls()
+    client2.set_debuglevel(True)
+
+    test_HELO(client2)
+    test_PING(client2)
+    test_AUTH(client2)
+    test_sendmail(client2)
+    print('client 2 end')
 
     controller.stop()
     print('tests successful')
